df347f25d7fef2c85f361ae975e71b0a0b14d01b
blasvicco@gmail.com <blasvicco@gmail.com>
2022-04-12 14:17:53 +0200

    Improve: Moving source file to the src folder.

--- src/ystable.jsx
+++ src/ystable.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { CsvToHtmlTable } from 'react-csv-to-table';
 
 export default class YsTable extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       csvData: '',
@@ -12,10 +12,10 @@ export default class YsTable extends React.Component {
   componentDidMount() {
     this.setState({
       hello: 'componentDidMount working nicely'
-    })
-    this.handleFiles()
+    });
+    this.handleFiles();
   }
-//  
+  //
   handleFiles = () => {
     let request = new XMLHttpRequest();
     //request.setRequestHeader('Content-Type', 'text');
@@ -29,29 +29,29 @@ export default class YsTable extends React.Component {
       let response = event.target.responseText;
       that.setState ({
         hello: 'state set inside EventListener'
-      })
+      });
       let doc = new DOMParser();
       let txt = doc.parseFromString(response, 'text/html');
-      console.error(txt.body)
+      console.error(txt.body);
       that.setState ({
         csvData: txt.body.innerHTML,
-      })
-    })
+      });
+    });
     request.send();
   }
 
   render () {
     return (
-      // React.createElement('h1', {}, this.state.hello); 
+      // React.createElement('h1', {}, this.state.hello);
       // React.createElement(
       //  'CsvToHtmlTable',
       //  {csvDelimiter: ",", tableClassName: "table table-striped table-hover", data: this.state.csvData }
       //)
       <CsvToHtmlTable data={this.state.csvData}
-      csvDelimiter=","
-      tableClassName="table table-striped table-hover" 
+        csvDelimiter=","
+        tableClassName="table table-striped table-hover"
       />
-      );
+    );
   }
 }
 
